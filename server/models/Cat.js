3088fea5f9f843337867d9f16fc53284301427ba
Michael Lynch <mpl2999@rit.edu>
2017-04-03 15:44:09 -0400

    Almost done adding dog

--- server/models/Cat.js
+++ server/models/Cat.js
@@ -6,38 +6,55 @@ let CatModel = {};
 
 const CatSchema = new mongoose.Schema({
   name: {
-    type: Strinng,
+    type: String,
     required: true,
     trim: true,
     unique: true,
   },
-  
+
   bedsOwned: {
     type: Number,
-     min: 0,
+    min: 0,
     required: true,
   },
-  
+
   createdDate: {
     type: Date,
     default: Date.now,
-  }
-  
+  },
+
 });
 
 CatSchema.statics.sayName = (cat) => {
   console.log(cat.name);
 };
 
-CatSchema.statics.findByName = (nameString, callback) => {
+// Schema.statics are static methods attached to the Model or objects
+// These DO NOT have their own instance. They are all the static function.
+// They do not look at individual instance variables since there is
+// no instance of them. Every static function
+// only exists once and is called.
+// In this case, findByName will be attached to the model and objects.
+// They will be able to call this function, but they won't be able to
+// reference any instance variables of that object (or at least accurately)
+// These are used when you want a public function you can call to do a task,
+// not a method that uses or returns instance variables
+// That is, these are used when you don't need an object, just a function to call.
+CatSchema.statics.findByName = (name, callback) => {
   const search = {
-    name: nameString
+    name,
   };
-  
+
   return CatModel.findOne(search, callback);
 };
 
-catModel = mongoose.model('Cat', CatSchema);
+// Create the cat model based on the schema. You provide it with a custom discriminator
+// (the name of the object type. Can be anything)
+// and the schema to make a model from.
+// Look at the model variable definition above for more details.
+CatModel = mongoose.model('Cat', CatSchema);
 
-module.exports.sayName = CatSchema.statics.sayName;
-module.exports.findByName = CatSchema.statics.findByName
+
+// export our public properties
+module.exports.CatModel = CatModel;
+module.exports.CatSchema = CatSchema;
---
+++ server/models/Dog.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+let DogModel = {};
+
+const DogSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  
+  breed: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  
+  age: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  
+  createdDate: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+DogSchema.statics.findByName = (dog, callback) => {
+  const search = {
+    name,
+  };
+  
+  return DogModel.findOne(search, callback);
+};
+
+DogModel = mongoose.model('Dog', DogSchema);
+
+module.exports.DogModel = DogModel;
+module.exports.DogSchema = DogSchema;
